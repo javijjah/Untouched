8ad8fdb476c40b0db46916e86ba261c5cda73bbf
Javier Jiménez-Alfaro Hacha <javilin7967@gmail.com>
2024-03-12 21:29:29 +0100

    Enemigo principal, animaciones y hitboxes básicas

--- Scripts/main_char.gd
+++ Scripts/main_char.gd
@@ -1,24 +1,45 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -400.0
+@onready var mc_sprite = $MCSprite
+@onready var mc_attack_area = $MCAttackArea
+#Variable que utilizaremos para que el ataque no se pueda spammear.
+#Hacemos que dependa de la animación para que el jugador no lo sienta injusto.
+var is_attacking = false
+#Sincronizamos la gravedad del proyecto con la que recibe este objeto.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _ready():
+	#Conectamos la señal del loop de la aplicación con la función que devuelve a la animación "Idle" en caso de que acabe el ataque
+	mc_sprite.animation_looped.connect(backToIdle)
 
+func _process(delta):
+	pass
+
+func _unhandled_input(event):
+	if Input.is_action_pressed("attack") and is_attacking==false:
+		mc_sprite.play("attack")
+		var bodiesInAttackRange = mc_attack_area.get_overlapping_bodies()
+		is_attacking=true
+		for body in bodiesInAttackRange:
+			print(body.name)
+			print(body)
+			body.die()
+
+func backToIdle():
+	if mc_sprite.animation=="attack":
+		mc_sprite.play("idle")
+		is_attacking=false
+		
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle jump.
+	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
--- Scripts/mushroom_enemy.gd
+++ Scripts/mushroom_enemy.gd
@@ -3,26 +3,39 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+@onready var player = get_node("/root/ForestScene/MainChar/MCCol")
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var enemy_sprite = $EnemySprite
 
+func _ready():
+	pass
+	# enemy_sprite.animation_looped.connect()
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	# Comienza el movimiento hacia el jugador
+	var direction = global_position.direction_to(player.global_position)
+	velocity.x = direction.x * 80.0
+	enemy_sprite.flip_h = direction.x < 0
+	move_and_slide()
+	
+func die():
+	enemy_sprite.play("hurt")
+	await get_tree().create_timer(0.5).timeout
+	queue_free()
+	## Handle jump.
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+#
+	## Get the input direction and handle the movement/deceleration.
+	## As good practice, you should replace UI actions with custom gameplay actions.
+	#var direction = Input.get_axis("ui_left", "ui_right")
+	#if direction:
+		#velocity.x = direction * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
