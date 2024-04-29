7aaa59608f3592148464cb8252ff21c56c8f555d
Javier Jiménez-Alfaro Hacha <javilin7967@gmail.com>
2024-04-30 00:36:52 +0200

    empiezo a trabagar en guardados

--- Scripts/SaveManager.gd
+++ Scripts/SaveManager.gd
@@ -1,27 +1,15 @@
-extends Node
+class_name SaveManager
+extends Resource
 
-var score = 0
+const SAVE_GAME_PATH := "user://savegame.tres"
+var Savedscore = 0
 func save_to_file(score):
-	var saveFile = [{
-		"score": score
-	}]
-	return saveFile
+	pass
 
-func save_game(score):
-	var save_game = FileAccess.open("user://savegame.save", FileAccess.WRITE)
-	var json_string = JSON.stringify(save_to_file(score))
-	save_game.store_line(json_string)
-
-func load_game():
-	if not FileAccess.file_exists("user://savegame.save"):
-		print ("isSaveFile")
-		
-	var save_game = FileAccess.open("user://savegame.save",FileAccess.READ)
-
-	while  save_game.get_position() < save_game.get_length():
-		var json_string = save_game.get_line()
-		var json = JSON.new()
-		var parse_result = json.parse(json_string)
-		var node_data = json.get_data()
-		print(node_data)
-		
+func save_game(score) -> void:
+	Savedscore = score
+	ResourceSaver.save(self,SAVE_GAME_PATH) #TODO seguir aquí para los guardados
+func load_game() -> Resource:
+	if ResourceLoader.exists(SAVE_GAME_PATH):
+		return load(SAVE_GAME_PATH)
+	return null
