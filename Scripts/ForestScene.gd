e968bfd3ed3e93a120f68e9f7ceb723dcfe492db
Javier Jiménez-Alfaro Hacha <javilin7967@gmail.com>
2024-06-05 09:38:06 +0200

    labelBugFixed

--- Scripts/ForestScene.gd
+++ Scripts/ForestScene.gd
@@ -26,7 +26,8 @@ func _ready():
 
 #parametro solo para no recibir error de consola por la señal
 func labelUpdate(dummy):
-	kill_counter.text = str(main_char.numberOfKills) #FIXME error al tener que sumarse cuando ya han matado al mainchar
+	if !main_char.isDead:
+		kill_counter.text = str(main_char.numberOfKills) #FIXME error al tener que sumarse cuando ya han matado al mainchar
 #endregion
 func barTotalUpdate(dummy):
 	augment_progress.changeTotal(main_char.xpToLevelUp)
